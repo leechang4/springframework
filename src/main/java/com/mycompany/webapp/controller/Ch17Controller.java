9c50f502fd6f6e5d619c581afc3d2badf90009ad
leechang4 <dlckdgus205@gmail.com>
2021-09-17 16:22:47 +0900

    0917 16:22 Ch17 최종 정리

--- src/main/java/com/mycompany/webapp/controller/Ch17Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch17Controller.java
@@ -10,8 +10,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
@@ -22,21 +20,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.mycompany.webapp.dto.Ch14Member;
 import com.mycompany.webapp.service.Ch14MemberService;
 import com.mycompany.webapp.service.Ch14MemberService.JoinResult;
+
 @Controller
 @RequestMapping("/ch17")
 public class Ch17Controller {
 	private static final Logger logger = LoggerFactory.getLogger(Ch17Controller.class);
-	
-	
+
 	@RequestMapping("/content")
-	public String content(Model model) {
+	public String content() {
 		logger.info("실행");
+
 		return "ch17/content";
 	}
 	
 	@RequestMapping("/loginForm")
 	public String loginForm() {
-		logger.info("실행");
 		return "ch17/loginForm";
 	}
 	
@@ -45,7 +43,7 @@ public class Ch17Controller {
 		logger.info("실행");
 		return "redirect:/ch17/content";
 	}
-	
+
 	@RequestMapping("/managerAction")
 	public String managerAction() {
 		logger.info("실행");
@@ -67,61 +65,61 @@ public class Ch17Controller {
 	@RequestMapping("/joinForm")
 	public String joinForm() {
 		logger.info("실행");
-		return"ch17/joinForm";
+		return "ch17/joinForm";
 	}
 	
 	@Resource
 	private Ch14MemberService memberService;
 	
-	
 	@RequestMapping("/join")
 	public String join(Ch14Member member, Model model) {
 		logger.info("실행");
 		
-		//활성화 설정
+		// 활성화 설정
 		member.setMenabled(1);
 		
-		//패스워드 암호화
+		// 패스워드 암호화
 		String mpassword = member.getMpassword();
-		BCryptPasswordEncoder passwordEncoder= new BCryptPasswordEncoder();
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		mpassword = "{bcrypt}" + passwordEncoder.encode(mpassword);
 		member.setMpassword(mpassword);
 		
-		//회원 가입 처리
-		JoinResult jr = memberService.join(member);
-		if(jr == JoinResult.SUCCESS) {
+		// 회원 가입 처리
+		JoinResult result = memberService.join(member);
+		
+		if (result == JoinResult.SUCCESS) {
 			return "redirect:/ch17/loginForm";
-		} else if(jr == JoinResult.DUPLICATED) {
+		} else if (result == JoinResult.DUPLICATED) { 
 			model.addAttribute("error", "중복된 아이디가 있습니다.");
 			return "ch17/joinForm";
-		} else {
+		} else { 
 			model.addAttribute("error", "회원 가입이 실패되었습니다. 다시 시도해 주세요.");
 			return "ch17/joinForm";
 		}
 	}
 	
-	@RequestMapping(value= "/userInfo", produces="application/json; charset=UTF-8")
+	@RequestMapping(value = "/userInfo", produces = "application/json; charset=UTF-8")
 	@ResponseBody
 	public String userInfo(Authentication authentication) {
 		logger.info("실행");
 		
-		//Spring Security가 인증 정보를 저장하는 컨테이너 객체를 얻기
-//		SecurityContext securityContext = SecurityContextHolder.getContext();
-		
-		//인증 정보 객체를 얻기
-//		Authentication authentication = securityContext.getAuthentication();
-		
-		//사용자 아이디를 얻가
+		/*		// Spring Security가 인증 정보를 저장하는 컨테이너 객체
+				SecurityContext securityContext = SecurityContextHolder.getContext(); // Holder: ~을 가지고 있는 자. 대부분 static 메서드 사용
+				
+				// 인증 정보 객체를 얻기
+				Authentication authentication = securityContext.getAuthentication(); // 인증 객체 얻어내기
+				*/
+		// 사용자 아이디 얻기
 		String mid = authentication.getName();
 		
-		//사용자 권한(롤) 이름 얻기
+		// 사용자 권한(role) 이름 얻기
 		List<String> authorityList = new ArrayList<>();
-		for(GrantedAuthority authority : authentication.getAuthorities()){
+		for(GrantedAuthority authority : authentication.getAuthorities()) { // 사용자가 권한을 여러개 가질 수 있다.
 			authorityList.add(authority.getAuthority());
 		}
 		
-		//사용자가 로그인한 PC의 IP 주소 얻기
-		WebAuthenticationDetails wad =(WebAuthenticationDetails) authentication.getDetails();
+		// 사용자가 로그인한 PC의 IP 주소 얻기
+		WebAuthenticationDetails wad = (WebAuthenticationDetails) authentication.getDetails();
 		String ip = wad.getRemoteAddress();
 		
 		JSONObject jsonObject = new JSONObject();
--- src/main/webapp/WEB-INF/views/ch17/content.jsp
+++ src/main/webapp/WEB-INF/views/ch17/content.jsp
@@ -1,31 +1,23 @@
-<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page contentType="text/html; charset=UTF-8"%>
+<%@ include file="/WEB-INF/views/common/header.jsp"%>
 
-<%@ include file="/WEB-INF/views/common/header.jsp" %>
-
-<div class= "card m-2 ">
-	<div class="card-header">
-		Spring Security
-	</div>
+<div class="card m-2">
+	<div class="card-header">Spring Security</div>
 	<div class="card-body">
-		<div class= "card">
-			<div class="card-header">
-				로그인/로그아웃
-			</div>
+		<div class="card">
+			<div class="card-header">로그인/로그아웃</div>
 			<div class="card-body">
 				<sec:authorize access="isAnonymous()">
 					<a href="loginForm" class="btn btn-info btn-sm">로그인</a>
 				</sec:authorize>
-			
+				
 				<sec:authorize access="isAuthenticated()">
-					<!-- 사이트간 요청 위조 방지가 비활성화 되어 있을 경우 -->
-					<%-- 
-					<a href="${pageContext.request.contextPath}/logout" 
-					class="btn btn-info btn-sm">로그아웃</a> 
-					--%>
+					<%-- 사이트간 요청 위조 방지가 비활성화 되어있을 경우 --%>
+					<%-- <a href="${pageContext.request.contextPath}/logout" class="btn btn-info btn-sm">로그아웃</a> --%>
 					
-					<!-- 사이트간 요청 위조 방지가 활성화 되어 있을 경우 -->
-					<form method="post" action="${pageContext.request.contextPath }/logout" class="d-inline-block;">
-						<input type ="hidden" name="${_csrf.parameterName }" value=${_csrf.token }/>
+					<%-- 사이트간 요청 위조 방지가 활성화 되어있을 경우 --%>
+					<form action="${pageContext.request.contextPath}/logout" method="post" class="d-inline-block">
+						<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
 						<button class="btn btn-info btn-sm">로그아웃</button>
 					</form>
 					
@@ -35,25 +27,24 @@
 				</sec:authorize>
 			</div>
 			<script>
-				function userInfo(){
+				function userInfo() {
 					$.ajax({
 						url: "userInfo"
-					}).done((data) =>{
-						//{mid:xxx, mrole:[yyy, ...], ip:zzz}
-					   var html="";
-					   html +="<p>Member ID " + data.mid + "</p>";
-					   html +="<p>Member Role: " + data.morle.toString() + "</p>";
-					   html +="<p>Member IP: " data.ip + "</p>";
-					   $("#userInfo").html(html);
+					}).done(data => {
+						//{mid: xxx, mrole: [yyy, ...], ip: zzz}
+						var html = "";
+						html += "<p>Member ID: " + data.mid + "</p>";
+						html += "<p>Member Role: "+ data.mrole.toString() +"</p>";
+						html += "<p>Member IP: "+ data.ip +"</p>";
+						$("#userInfo").html(html);
 					});
+			
 				}
 			</script>
 		</div>
 		
-		<div class= "card">
-			<div class="card-header">
-				접근 권한
-			</div>
+		<div class="card">
+			<div class="card-header">접근 권한</div>
 			<div class="card-body">
 				<a href="adminAction" class="btn btn-info btn-sm">Admin Action</a>
 				<a href="managerAction" class="btn btn-info btn-sm">Manager Action</a>
@@ -63,26 +54,24 @@
 					<sec:authorize access="hasRole('ROLE_ADMIN')">
 						<li>Admin Menu</li>
 					</sec:authorize>
-					
 					<sec:authorize access="hasRole('ROLE_MANAGER')">
 						<li>Manager Menu</li>
 					</sec:authorize>
-					
-					<sec:authorize access="hasRole('ROLE_USER')">
+					<%-- <sec:authorize access="isAuthenticated()">와 동일 --%>
+					<sec:authorize access="hasRole('ROLE_USER')"> 
 						<li>User Menu</li>
 					</sec:authorize>
 				</ul>
 			</div>
 		</div>
 		
-		<div class= "card">
-			<div class="card-header">
-				회원 가입(비밀 번호 암호화)
-			</div>
+		<div class="card">
+			<div class="card-header">회원 가입(비밀번호 암호화)</div>
 			<div class="card-body">
-				<a href="joinForm" class = "btn btn-info btn-sm">회원가입</a>
+				<a href="joinForm" class="btn btn-info btn-sm">회원 가입</a>
 			</div>
 		</div>
+		
 	</div>
 </div>
 
